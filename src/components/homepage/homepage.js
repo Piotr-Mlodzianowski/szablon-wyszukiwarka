699b4cedb12f8f6a843468558180674c82b30f52
unknown <piotrmlodz@gmail.com>
2023-09-20 16:10:55 +0200

    Opening component with scss styles

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Menu from './menu/menu';
+import Opening from "./opening/opening";
 
 const Homepage = () => {
 	return (
 		<>
 			<Menu />
+			<Opening/>
 		</>
 	)
 }
---
+++ src/components/homepage/opening/opening.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Wave from 'react-wavify'
+
+const Opening = () => {
+	return (
+		<>
+			<section className='opening' id='opening'>
+				<div className='opening__content'>
+					<h1 className='content__heading'>Szablon aplikacji do wyszukiwania mediów</h1>
+
+					<p className='content__paragraph'>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et tristique justo, ut volutpat.
+					</p>
+
+					<div className='content__buttons'>
+						<a href='#history' className='content__button'>
+							O nas
+						</a>
+						<a href='#searchApp' className='content__button'>
+							Wyszukiwarka
+						</a>
+					</div>
+				</div>
+				<img
+					className='opening__image'
+					src={require('../../../images/PinClipart.com_dvd-clip-art_5376605.png')}
+					alt='Compact disc'
+				/>
+			</section>
+			<Wave
+				fill='#5E81AC'
+				paused={false}
+				style={{ display: 'flex' }}
+				options={{ height: 80, amplitude: 20, speed: 0.10, points: 20 }}
+			/>
+		</>
+	);
+};
+
+export default Opening;
