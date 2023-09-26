610f38b3b87d6499aac2bf6f8c52808b1d1a9371
unknown <piotrmlodz@gmail.com>
2023-09-26 14:51:15 +0200

    Added routing to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import {BrowserRouter} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import {DataProvider} from './DataContext';
 import Homepage from './components/homepage/homepage'
+import SearchApp from "./components/searchApp/searchApp";
 
 function App() {
-	return (
-		<>
-			<DataProvider>
-				<Homepage />
-			</DataProvider>
-		</>
-	)
+    return (
+        <>
+            <DataProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Homepage/>}/>
+                        <Route path="searchApp" element={<SearchApp/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </DataProvider>
+        </>
+    )
 }
 
 export default App
