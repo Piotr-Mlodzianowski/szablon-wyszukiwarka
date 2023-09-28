a1936ba12beae40cb30aedcaf553d6d2f48f827a
unknown <piotrmlodz@gmail.com>
2023-09-28 09:32:43 +0200

    Added logic for search filtering

--- src/components/searchApp/searchWindow/searchWindow.js
+++ src/components/searchApp/searchWindow/searchWindow.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {DataContext} from "../../../DataContext";
 
 const SearchWindow = () => {
 
@@ -23,8 +24,11 @@ const SearchForm = () => {
 }
 
 const SearchBar = () => {
+    const {setEnteredTitle} = useContext(DataContext);
+
+
     return (
-        <input className='form__input' type='text' placeholder='wpisz tytuł'/>
+        <input className='form__input' type='text' placeholder='wpisz tytuł' onChange={(e) => setEnteredTitle(e.target.value)}/>
     )
 }
 
@@ -39,18 +43,20 @@ const SearchButtons = () => {
 
 
 const SearchRadios = () => {
+    const {setChosenFilter} = useContext(DataContext);
+
     return (
         <div className='radios'>
             <div className='radio__container'>
-                <input className='radio' type="radio" id="all" name="platform" value="all"/>
+                <input className='radio' type="radio" id="all" name="platform" value="all" onChange={(e) => setChosenFilter(e.target.value)}/>
                 <label className='radio__label' htmlFor="all">Wszystkie</label>
             </div>
             <div className='radio__container'>
-                <input className='radio' type="radio" id="filter1" name="platform" value="filter1"/>
+                <input className='radio' type="radio" id="filter1" name="platform" value="filter1" onChange={(e) => setChosenFilter(e.target.value)}/>
                 <label className='radio__label' htmlFor="filter1">Filtr 1</label>
             </div>
             <div className='radio__container'>
-                <input className='radio' type="radio" id="filter2" name="platform" value="filter2"/>
+                <input className='radio' type="radio" id="filter2" name="platform" value="filter2" onChange={(e) => setChosenFilter(e.target.value)}/>
                 <label className='radio__label' htmlFor="filter2">Filtr 2</label>
             </div>
         </div>
