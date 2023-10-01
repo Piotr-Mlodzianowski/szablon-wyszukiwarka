34875f9ab26dfcac10fab1f3056c3785b0ffefd3
unknown <piotrmlodz@gmail.com>
2023-10-01 12:07:12 +0200

    searchApp prepared for pagination

--- src/components/searchApp/searchApp.js
+++ src/components/searchApp/searchApp.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React ,{useContext} from 'react';
+import {DataContext} from "../../DataContext";
 import SearchAppMenu from "./searchAppMenu/searchAppMenu";
 import SearchWindow from "./searchWindow/searchWindow";
 import SearchList from "./searchList/searchList";
 
 const SearchApp = () => {
+    const {
+        allMedia,
+        chosenFilter,
+        enteredTitle,
+    } = useContext(DataContext);
+
+    let filteredByName = allMedia;
+    if (enteredTitle !== "") {
+        filteredByName = allMedia.filter((media) => media.title.toUpperCase().includes(enteredTitle.toUpperCase()));
+    }
+
+    let filteredByNameAndFilter = filteredByName;
+    if (chosenFilter !== "ALL") {
+        filteredByNameAndFilter = filteredByName.filter((media) => media.platform === chosenFilter);
+    }
 
     return (
         <>
