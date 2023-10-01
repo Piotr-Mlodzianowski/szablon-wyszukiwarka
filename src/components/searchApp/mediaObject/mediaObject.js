87450e7a554f1f3b7e85564433c40b9b304ad844
unknown <piotrmlodz@gmail.com>
2023-10-01 12:18:49 +0200

    corrected mediaObject structure

--- src/components/searchApp/mediaObject/mediaObject.js
+++ src/components/searchApp/mediaObject/mediaObject.js
@@ -1,37 +1,31 @@
 const media = [
     {
         title: "Maecenas",
-        cover: <img src={require("../../../images/example/image-from-rawpixel-id-9199110-jpeg.jpg")} alt="Maecenas cover"
-                    className='media__cover'/>,
+        cover: require("../../../images/example/image-from-rawpixel-id-9199110-jpeg.jpg"),
         platform: "filter1",
         id: 1
     },
     {
         title: "quis",
-        cover: <img src={require("../../../images/example/image-from-rawpixel-id-8668923-jpeg.jpg")}
-                    alt="quis cover" className='media__cover'/>,
+        cover: require("../../../images/example/image-from-rawpixel-id-8668923-jpeg.jpg"),
         platform: "filter1",
         id: 2
     },
     {
         title: "dui",
-        cover: <img
-            src={require("../../../images/example/image-from-rawpixel-id-8649458-jpeg.jpg")} alt="dui cover"
-            className='media__cover'/>,
+        cover: require("../../../images/example/image-from-rawpixel-id-8649458-jpeg.jpg"),
         platform: "filter1",
         id: 3
     },
     {
         title: "fringilla",
-        cover: <img src={require("../../../images/example/image-from-rawpixel-id-8649370-jpeg.jpg")}
-                    alt="fringilla cover" className='media__cover'/>,
+        cover: require("../../../images/example/image-from-rawpixel-id-8649370-jpeg.jpg"),
         platform: "filter2",
         id: 4
     },
     {
         title: "malesuada",
-        cover: <img src={require("../../../images/example/image-from-rawpixel-id-8649331-jpeg.jpg")}
-                    alt="malesuada cover" className='media__cover'/>,
+        cover: require("../../../images/example/image-from-rawpixel-id-8649331-jpeg.jpg"),
         platform: "filter2",
         id: 5
     }
