512e759a7a862879fc020b09cecf1f068c3d43c5
unknown <piotrmlodz@gmail.com>
2023-10-01 12:06:15 +0200

    modified searchList structure, code and styles for future pagination

--- src/components/searchApp/searchList/searchList.js
+++ src/components/searchApp/searchList/searchList.js
@@ -2,30 +2,20 @@ import React, {useContext} from 'react';
 import {DataContext} from "../../../DataContext";
 
 const SearchList = () => {
-    const {allMedia, chosenFilter, enteredTitle, selectedMedia, setSelectedMedia} = useContext(DataContext);
-
-    let filteredByFilter = allMedia
-    if (chosenFilter !== "all") {
-        filteredByFilter = allMedia.filter((media) => media.platform === chosenFilter);
-    }
-
-    let filteredByFilterAndName = filteredByFilter;
-    if (enteredTitle !== "") {
-        filteredByFilterAndName = filteredByFilter.filter((media) => media.title.toUpperCase().includes(enteredTitle.toUpperCase()));
-    }
-
+    const {selectedMedia, setSelectedMedia, currentItems} = useContext(DataContext);
     window.localStorage.setItem("media", JSON.stringify(selectedMedia));
-
 
     return (
         <div className="container">
-            <div className='list__content'>
-                <div className='mediaList'>
-                    {filteredByFilterAndName.map((media) =>
-                        <a href="/wybraneMedia" className="media" key={media.id} onMouseOver={() => setSelectedMedia(media)}>
-                            {media.cover}
-                        </a>)}
-                </div>
+            <div className='media'>
+                {currentItems.map((media) =>
+                    <a href="/wybranaGra" className="selected" key={media.id} onClick={() => setSelectedMedia(media)}>
+                        <img src={media.cover} alt="media cover" className='media__cover'/>
+                        <div className='media__info'>
+                            <div className='title'>{media.title}</div>
+                            <div className='more'>więcej</div>
+                        </div>
+                    </a>)}
             </div>
         </div>
     )
