c1a4e08a67d1dcf010497049d3a60d0610244833
unknown <piotrmlodz@gmail.com>
2023-10-02 15:41:19 +0200

    added new information to mediaObject

--- src/components/searchApp/mediaObject/mediaObject.js
+++ src/components/searchApp/mediaObject/mediaObject.js
@@ -2,32 +2,82 @@ const media = [
     {
         title: "Maecenas",
         cover: require("../../../images/example/image-from-rawpixel-id-9199110-jpeg.jpg"),
+        artwork: require("../../../images/example/image-from-rawpixel-id-9199110-jpeg.jpg"),
+        movie: "https://www.pexels.com/video/wood-dawn-landscape-sunset-17875526/",
         platform: "filter1",
         id: 1
     },
     {
         title: "quis",
         cover: require("../../../images/example/image-from-rawpixel-id-8668923-jpeg.jpg"),
+        artwork: require("../../../images/example/image-from-rawpixel-id-8668923-jpeg.jpg"),
+        movie: "https://www.pexels.com/video/wood-dawn-landscape-sunset-17875526/",
         platform: "filter1",
         id: 2
     },
     {
         title: "dui",
         cover: require("../../../images/example/image-from-rawpixel-id-8649458-jpeg.jpg"),
+        artwork: require("../../../images/example/image-from-rawpixel-id-8649458-jpeg.jpg"),
+        movie: "https://www.pexels.com/video/wood-dawn-landscape-sunset-17875526/",
         platform: "filter1",
         id: 3
     },
     {
         title: "fringilla",
         cover: require("../../../images/example/image-from-rawpixel-id-8649370-jpeg.jpg"),
+        artwork: require("../../../images/example/image-from-rawpixel-id-8649370-jpeg.jpg"),
+        movie: "https://www.pexels.com/video/wood-dawn-landscape-sunset-17875526/",
         platform: "filter2",
         id: 4
     },
     {
         title: "malesuada",
         cover: require("../../../images/example/image-from-rawpixel-id-8649331-jpeg.jpg"),
+        artwork: require("../../../images/example/image-from-rawpixel-id-8649331-jpeg.jpg"),
+        movie: "https://www.pexels.com/video/wood-dawn-landscape-sunset-17875526/",
         platform: "filter2",
         id: 5
+    },
+    {
+        title: "Maecenas",
+        cover: require("../../../images/example/image-from-rawpixel-id-9199110-jpeg.jpg"),
+        artwork: require("../../../images/example/image-from-rawpixel-id-9199110-jpeg.jpg"),
+        movie: "https://www.pexels.com/video/wood-dawn-landscape-sunset-17875526/",
+        platform: "filter1",
+        id: 6
+    },
+    {
+        title: "quis",
+        cover: require("../../../images/example/image-from-rawpixel-id-8668923-jpeg.jpg"),
+        artwork: require("../../../images/example/image-from-rawpixel-id-8668923-jpeg.jpg"),
+        movie: "https://www.pexels.com/video/wood-dawn-landscape-sunset-17875526/",
+        platform: "filter1",
+        id: 7
+    },
+    {
+        title: "dui",
+        cover: require("../../../images/example/image-from-rawpixel-id-8649458-jpeg.jpg"),
+        artwork: require("../../../images/example/image-from-rawpixel-id-8649458-jpeg.jpg"),
+        movie: "https://www.pexels.com/video/wood-dawn-landscape-sunset-17875526/",
+        platform: "filter1",
+        id: 8
+    },
+    {
+        title: "fringilla",
+        cover: require("../../../images/example/image-from-rawpixel-id-8649370-jpeg.jpg"),
+        artwork: require("../../../images/example/image-from-rawpixel-id-8649370-jpeg.jpg"),
+        movie: "https://www.pexels.com/video/wood-dawn-landscape-sunset-17875526/",
+        platform: "filter2",
+        id: 9
+    },
+    {
+        title: "malesuada",
+        cover: require("../../../images/example/image-from-rawpixel-id-8649331-jpeg.jpg"),
+        artwork: require("../../../images/example/image-from-rawpixel-id-8649331-jpeg.jpg"),
+        movie: "https://www.pexels.com/video/wood-dawn-landscape-sunset-17875526/",
+        platform: "filter2",
+        id: 10
     }
 ]
 
